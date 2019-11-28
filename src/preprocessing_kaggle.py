181f40d0204692e71534d9b89f76ba8134a46434
Nikita Surkov <surkov.nikita.a@gmail.com>
2019-11-28 15:06:50 +0300

    Расширение фич в данные kaggle.

--- src/preprocessing_kaggle.py
+++ src/preprocessing_kaggle.py
@@ -6,12 +6,13 @@ import numpy as np
 import pickle
 import random
 import shutil
+import pandas as pd
 
 def make_subset(kaggle_data_path, users_num, force=False, random_seed=None):
     if random_seed is not None:
         random.seed(random_seed)
 
-    site_freq_path = os.path.join(kaggle_data_path, 'site_dic_mod.pkl')
+    site_freq_path = os.path.join(kaggle_data_path, 'site_freq.pkl')
     sessions_path = os.path.join(kaggle_data_path, 'train_sessions_u{}.csv'.format(users_num))
     users_path = os.path.join(kaggle_data_path, 'train', 'other_user_logs')
 
@@ -48,19 +49,44 @@ if __name__ == '__main__':
     src_folder = os.path.join('.', '..', 'data', 'kaggle')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('stages', nargs='?', default='all', help='preprocessing stages')
-    parser.add_argument('subset_len', nargs='?', default=10, help='len of users subset')
+    parser.add_argument('subset_len', nargs='?', default=None, help='len of users subset')
     parser.add_argument('data_path', nargs='?', default=src_folder, help='directory with kaggle data')
 
     args = parser.parse_args()
 
     PATH_TO_DATA = args.data_path
 
-    if args.stages == 'all':
-        ppstages = [i for i in range(3)]
+    if args.subset_len is not None:
+        un = int(args.subset_len)
+        make_subset(PATH_TO_DATA, un, random_seed=17)
+        sessions_path = os.path.join(PATH_TO_DATA, 'train_sessions_u{}.csv'.format(un))
+        fext_path = os.path.join(PATH_TO_DATA, 'train_sessions_fext_u{}.csv'.format(un))
     else:
-        ppstages = [int(args.stages)]
+        sessions_path = os.path.join(PATH_TO_DATA, 'train_sessions.csv')
+        fext_path = os.path.join(PATH_TO_DATA, 'train_sessions_fext.csv')
 
-    subset_length = int(args.subset_len)
-    if 1 in ppstages:
-        make_subset(PATH_TO_DATA, subset_length, random_seed=17)
+
+    features = ['session_timespan', '#unique_sites', 'start_hour',
+                'day_of_week', 'timespan_median', 'timespan_mean',
+                'daily_aсtivity', 'freq_facebook', 'timespan_youtube',
+                'timespan_mail', 'freq_googlevideo', 'freq_google']
+
+    time_cols = ['time%d' % i for i in range(1, 11)]
+    sessions = pd.read_csv(sessions_path,
+                           index_col='session_id',
+                           parse_dates=time_cols).rename(columns={'target': 'user_id'})
+    train_data = prepare_train_set_fe(sessions,
+                                      site_freq_path=os.path.join(PATH_TO_DATA, 'site_freq.pkl'),
+                                      feature_names=features)
+
+    columns = ['session_timespan',
+               '#unique_sites',
+               'start_hour',
+               'day_of_week',
+               'timespan_mail',
+               'freq_googlevideo',
+               'target']
+    new_features = train_data[columns]
+
+    with open(fext_path, 'wb') as fo:
+        pickle.dump(new_features, fo)
